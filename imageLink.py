ddeca0d5bf8b677e70c2a15c32b2e353e7b00e9d
msakib162004 <70893762+msakib162004@users.noreply.github.com>
2021-12-19 12:22:20 +0000

    First Commit

---
+++ imageLink.py
@@ -0,0 +1,14 @@
+from html.parser import HTMLParser
+import urllib.request
+from pprint import pprint
+from itertools import count
+import os
+import json
+import shelve
+from contextlib import contextmanager
+
+url = 'https://www.vrbo.com/vacation-rentals/condos-and-apartments/usa/florida/north-west/panama-city-beach'
+req = urllib.request.Request(url, headers={'User-Agent': 'Mozila'})
+
+with urllib.request.urlopen(req) as html:
+  page = html.read()
